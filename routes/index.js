f90191a8c453e24c2cbc898c1954b3edc1eaa3c5
Miguel Sánchez Martín <miguel.sanchez.martin@alumnos.upm.es>
2016-04-25 20:11:00 +0200

    Página author

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
+})
+
+router.get('/author', function(req, res, next) {
+  res.render('author', { title: 'Quiz' });
+  res.render('author', {authors: 'Miguel Sánchez Martín y José María Carmona Pérez'});
 });
 
 module.exports = router;
