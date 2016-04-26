cf62c9738f93f1499ebf2529e69b50f79dfe386b
Miguel Sánchez Martín <miguel.sanchez.martin@alumnos.upm.es>
2016-04-26 19:31:46 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
-})
+});
+
 
 router.get('/author', function(req, res, next) {
   res.render('author', { title: 'Quiz' });
-  res.render('author', {authors: 'Miguel Sánchez Martín y José María Carmona Pérez'});
+  //res.render('author', {authors: 'Miguel Sánchez Martín y José María Carmona Pérez'});
 });
+
+router.get('/question', quizController.question);
+router.get('/check', quizController.check);
+
+/*router.get('/question', function(req, res, next) {
+  res.render('question', { title: 'Quiz' });
+  //res.render('author', {authors: 'Miguel Sánchez Martín y José María Carmona Pérez'});
+});
+
+router.get('/result', function(req, res, next) {
+  res.render('result', { title: 'Quiz' });
+  //res.render('author', {authors: 'Miguel Sánchez Martín y José María Carmona Pérez'});
+});*/
+
+
 
 module.exports = router;
