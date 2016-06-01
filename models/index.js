02fb7ea229bc5d4ba6bc228aab0d1f40bebd2d71
Miguel Sánchez Martín <miguel.sanchez.martin@alumnos.upm.es>
2016-06-01 18:42:06 +0200

    17 heroku

--- models/index.js
+++ models/index.js
@@ -1,25 +1,54 @@
 var path = require('path');
 
+// Cargar ORM
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(null,null,null,
-								{dialect: "sqlite", storage: "quiz.sqlite"});
+// Usar BBDD SQLite:
+//    DATABASE_URL = sqlite:///
+//    DATABASE_STORAGE = quiz.sqlite
+// Usar BBDD Postgres:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
 
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var url, storage;
 
-sequelize.sync().then(function(){
-	return Quiz.count().then(function(c){
-		if(c===0){
-			return Quiz.bulkCreate([{question: 'Capital de Italia', answer: 'Roma'},
-									{question: 'Capital de Portugal', answer: 'Lisboa'}
-									]).then(function(){
-				console.log('Base de datos inicializada con datos');
-			});
-		}
-	});
-}).catch(function(error){
-	console.log("Error sincronizando las tablas de la BBDD", error);
-	process.exit(1);
-});
+if (!process.env.DATABASE_URL) {
+    url = "sqlite:///";
+    storage = "quiz.sqlite";
+} else {
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
+}
 
-exports.Quiz = Quiz;
+var sequelize = new Sequelize(url, 
+	 						  { storage: storage,
+				              	omitNull: true 
+				              });
+
+// Importar la definicion de la tabla Quiz de quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+
+/* USARE Migraciones y Seeders
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync()
+    .then(function() {
+        // Ya se han creado las tablas necesarias.
+        return Quiz.count()
+                .then(function (c) {
+                    if (c === 0) {   // la tabla se inicializa solo si está vacía
+                        return Quiz.bulkCreate([ {question: 'Capital de Italia',   answer: 'Roma'},
+                                                 {question: 'Capital de Portugal', answer: 'Lisboa'}
+                                              ])
+                                   .then(function() {
+                                        console.log('Base de datos inicializada con datos');
+                                    });
+                    }
+                });
+    })
+    .catch(function(error) {
+        console.log("Error Sincronizando las tablas de la BBDD:", error);
+        process.exit(1);
+    });
+*/
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
